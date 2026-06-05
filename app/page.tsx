528c52b40df027d6c9dc7022f272aaca8e87b9ba
Brian Kim <brian.kyounghoon.kim@gmail.com>
2026-06-05 17:06:47 -0400

    landing page placement added for full flow

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default async function Home() {
     <div >
       {/* <Header /> */}
       <h1 className="absolute font-bold top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-[10rem] flex flex-col items-center leading-[100%]">
-        <span>CREATIVE</span>
+        <span>KINECT</span>
         <span>CMS</span>
       </h1>
     </div>
