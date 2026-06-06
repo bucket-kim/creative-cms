1c31b044045cd358429d249d45e168144db94784
Brian Kim <brian.kyounghoon.kim@gmail.com>
2026-06-05 21:32:48 -0400

    dark theme bug fixed

--- app/components/Background/Background.tsx
+++ app/components/Background/Background.tsx
@@ -5,27 +5,30 @@ import { Blob, DotGrid, FilmGrain, LinearGradient, Liquify, Shader } from 'shade
 
 const Background = () => {
 
-    const { theme } = useTheme()
+    const { resolvedTheme } = useTheme()
+    const isDark = resolvedTheme === 'dark'
 
     return (
         <Shader className='w-full h-full fixed inset-0 -z-10'>
-            <LinearGradient colorA={theme === 'dark' ? "#27282a" : "#f2f4ff"}
-                colorB={theme === 'dark' ? "#1d1f20" : "#eef6ff"}
+            <LinearGradient colorA={isDark ? "#1a1b1c" : "#f2f4ff"}
+                colorB={isDark ? "#121415" : "#eef6ff"}
                 edges='mirror'
                 colorSpace="oklch" />
-            <Blob blendMode='normal-oklch' colorA={theme === 'dark' ? "#292a2e" : "#e5e9fd"}
-                colorB={theme === 'dark' ? "#2a2d30" : "#dceafa"}
+            <Blob blendMode='normal-oklch' colorA={isDark ? "#292a2e" : "#e5e9fd"}
+                colorB={isDark ? "#2a2d30" : "#dceafa"}
                 softness={2}
                 colorSpace="oklch" />
             <Liquify />
             <DotGrid
-                color="#f2f4fc"
+                color={isDark ? "#252527 " : "#f2f4fc"}
                 density={50}
                 dotSize={0.15}
                 blendMode="screen"
                 opacity={0.5}
             />
-            <FilmGrain />
+            {isDark ? null : (
+                <FilmGrain />
+            )}
         </Shader>
     )
 }
